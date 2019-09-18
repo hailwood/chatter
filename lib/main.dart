37c3bd60bafcde7e7d65d77deebdf8191cd813b4
Matthew Hailwood <matthew@hailwood.nz>
2019-09-18 23:07:11 +1200

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Chatter'),
+      home: MyHomePage(title: 'Chatter Messaging'),
     );
   }
 
